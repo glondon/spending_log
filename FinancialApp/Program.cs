649cfac507ee97b2afb1a5e48ca202c3114dacc5
Greg London <gregdlondon@gmail.com>
2017-07-30 01:32:29 -0500

    Set up DB and reading data successfully

--- FinancialApp/Program.cs
+++ FinancialApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace FinancialApp
 {
@@ -10,6 +11,18 @@ namespace FinancialApp
     {
         static void Main(string[] args)
         {
+            SqlConnection conn = new SqlConnection("Data Source=GREG-BEE-2;Initial Catalog=spending;Integrated Security=True");
+            SqlDataReader rdr = null;
+ 
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM personal", conn);
+            rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+                Console.WriteLine(rdr[0] + " $" + rdr[1]);
+
+            Console.ReadLine();
+            
         }
     }
 }
