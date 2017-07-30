e3a4d2b5cf41f729e9968c8eb591011185d1377c
Greg London <gregdlondon@gmail.com>
2017-07-30 13:14:54 -0500

    Retrieved int input and validated

--- FinancialApp/Program.cs
+++ FinancialApp/Program.cs
@@ -14,6 +14,8 @@ namespace FinancialApp
             Console.WriteLine("Choose one of the following:\n");
             Console.WriteLine("1. Show all spending activity for current month");
             Console.WriteLine("2. Show all spending activity summarized for current month");
+
+            Console.WriteLine("\n");
         }
 
         static void Main(string[] args)
@@ -21,6 +23,28 @@ namespace FinancialApp
             Console.WriteLine("\n---------- Financial App - Personal Spending Summary ----------\n");
             Program p = new Program();
             p.menu();
+            string menuItem = Console.ReadLine();
+            int intCheck;
+            if (int.TryParse(menuItem, out intCheck))
+            {
+                intCheck = Int32.Parse(menuItem);
+
+                switch (intCheck)
+                {
+                    case 1:
+                        Console.WriteLine("You chose 1");
+                        break;
+                    case 2:
+                        Console.WriteLine("You chose 2");
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid choice");
+                        break;
+                }
+            }
+            else
+                Console.WriteLine("Not a valid integer\n");
+            
 
             SqlConnection conn = new SqlConnection("Data Source=GREG-BEE-2;Initial Catalog=spending;Integrated Security=True");
             SqlDataReader rdr = null;
@@ -28,6 +52,7 @@ namespace FinancialApp
             conn.Open();
             SqlCommand cmd = new SqlCommand("SELECT * FROM personal", conn);
             rdr = cmd.ExecuteReader();
+            Console.WriteLine("\n");
 
             while (rdr.Read())
                 Console.WriteLine(rdr[0] + " $" + rdr[1]);
