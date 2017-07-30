ac7ca2df96cf5674810cc76a791acbc9523c81fa
Greg London <gregdlondon@gmail.com>
2017-07-30 13:41:06 -0500

    Add intial case 1 logic

--- FinancialApp/Program.cs
+++ FinancialApp/Program.cs
@@ -14,6 +14,7 @@ namespace FinancialApp
             Console.WriteLine("Choose one of the following:\n");
             Console.WriteLine("1. Show all spending activity for current month");
             Console.WriteLine("2. Show all spending activity summarized for current month");
+            Console.WriteLine("9. Quit program");
 
             Console.WriteLine("\n");
         }
@@ -21,9 +22,18 @@ namespace FinancialApp
         static void Main(string[] args)
         {
             Console.WriteLine("\n---------- Financial App - Personal Spending Summary ----------\n");
+
+            SqlConnection conn = new SqlConnection("Data Source=GREG-BEE-2;Initial Catalog=spending;Integrated Security=True");
+            SqlDataReader rdr = null;
+            conn.Open();
+
             Program p = new Program();
             p.menu();
+
             string menuItem = Console.ReadLine();
+
+            //TODO add while statement for input loop
+            
             int intCheck;
             if (int.TryParse(menuItem, out intCheck))
             {
@@ -32,10 +42,26 @@ namespace FinancialApp
                 switch (intCheck)
                 {
                     case 1:
-                        Console.WriteLine("You chose 1");
+                        Console.WriteLine("Spending for Current Month:\n");
+                        DateTime today = DateTime.Today;
+                        Console.WriteLine(today.ToString("d")); //TODO need to get entire month
+                        SqlCommand cmd = new SqlCommand("SELECT * FROM personal WHERE date = '" + today.ToString("d") + "' ORDER BY date", conn);
+                        rdr = cmd.ExecuteReader();
+                        Console.WriteLine("\n");
+                        if (rdr.HasRows)
+                        {
+                            while (rdr.Read())
+                                Console.WriteLine(rdr[0] + " $" + rdr[1] + " " + rdr[2] + " " + rdr[3]);
+                        }
+                        else
+                            Console.WriteLine("No results");
+                        
                         break;
                     case 2:
                         Console.WriteLine("You chose 2");
+                        break;
+                    case 9:
+                        Environment.Exit(0);
                         break;
                     default:
                         Console.WriteLine("Not a valid choice");
@@ -45,17 +71,6 @@ namespace FinancialApp
             else
                 Console.WriteLine("Not a valid integer\n");
             
-
-            SqlConnection conn = new SqlConnection("Data Source=GREG-BEE-2;Initial Catalog=spending;Integrated Security=True");
-            SqlDataReader rdr = null;
- 
-            conn.Open();
-            SqlCommand cmd = new SqlCommand("SELECT * FROM personal", conn);
-            rdr = cmd.ExecuteReader();
-            Console.WriteLine("\n");
-
-            while (rdr.Read())
-                Console.WriteLine(rdr[0] + " $" + rdr[1]);
 
             rdr.Close();
             conn.Close();
