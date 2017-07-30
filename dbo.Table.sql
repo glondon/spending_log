d73dddf095b78c1beb5afbbd09df4d254615fe7e
Greg London <gregdlondon@gmail.com>
2017-07-29 23:44:27 -0500

    DB table sql for expenses

---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[personal]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [cost] DECIMAL(18, 2) NOT NULL, 
+    [category] VARCHAR(50) NOT NULL, 
+    [payment_type] VARCHAR(50) NOT NULL, 
+    [date] DATE NOT NULL
+)
